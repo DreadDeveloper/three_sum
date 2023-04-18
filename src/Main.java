61cd2b0824abab2f6f57e72272df6d9cf9ccb4c0
Abhinav Kumar <kumaraka2000@gmail.com>
2023-04-18 19:09:27 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        for(List list : solution.threeSum(nums))
+        {
+            System.out.println(list);
+        }
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Solution {
+import java.util.*;
+public class Solution
+{
+    public List<List<Integer>> threeSum(int[] nums)
+    {
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<>();
+        int j, k;
+        for(int i = 0; i < nums.length - 2; i++)
+        {
+            j = i + 1;
+            k = nums.length - 1;
+            while(j < k)
+            {
+                if(nums[i] + nums[j] + nums[k] == 0)
+                {
+
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    set.add(list);
+                    j++;
+                }
+                else if(nums[i] + nums[j] + nums[k] < 0)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
 }
